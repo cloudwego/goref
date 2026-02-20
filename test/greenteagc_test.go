4a55deebbe410202f4c8d634ebbb0280c339a17d
qiheng.zhou <qiheng.zhou@bytedance.com>
2026-02-20 10:44:15 +0800

    fix: ci

--- test/greenteagc_test.go
+++ test/greenteagc_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.26 && goexperiment.greenteagc
+//go:build goexperiment.greenteagc
 
 package test
 
